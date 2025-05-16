b87a2c88c3799f06f5319dfe8cce4be9ac418a36
Muhammad Osama <muhammadosama0080@gmail.com>
2025-05-17 00:51:30 +0500

    add API route to delete review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -47,4 +47,27 @@ const getAllReviews = async (req, res) => {
   }
 };
 
-module.exports = { createReview, getAllReviews };
+const deleteReview = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const response = await User.findByIdAndDelete(id);
+
+    if (!response) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Record not found." });
+    }
+
+    res
+      .status(200)
+      .json({ success: true, message: "Feedback deleted successfully." });
+  } catch (error) {
+    console.error("Error Deleting: ", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error.",
+    });
+  }
+};
+
+module.exports = { createReview, getAllReviews, deleteReview };
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const {
   createReview,
   getAllReviews,
+  deleteReview,
 } = require("../controllers/reviewController");
 
 router.post("/", createReview);
 
 router.get("/", getAllReviews);
+
+router.delete("/:id", deleteReview);
 
 module.exports = router;
